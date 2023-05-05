5b7fdf123fbb431a3663f45129ae1308a50bf21d
unknown <virat.18.12345@gmail.com>
2023-05-05 14:28:46 -0400

    test

--- src/consumer.ts
+++ src/consumer.ts
@@ -8,8 +8,8 @@ export type CloudWatchLogsConsumerOptions = {
   retentionInDays?: number;
 };
 
-const MAX_RECORDS = 5000;
-const MAX_SIZE = 1024 * 800; // 800 KB
+const MAX_RECORDS = 25000;
+const MAX_SIZE = 1024 * 8000; // 8000 KB
 const MAX_DELAY = 5000; // 5 sec
 
 export class CloudWatchLogsConsumer {
